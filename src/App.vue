ef34770452a8c1dffa481026b09b2a9edba84614
Paula Belmiro <paulabelmiro@yahoo.com.br>
2023-02-28 07:58:04 -0300

    inserido formulario

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav class="ml-auto" >
-        <b-nav-item href="#" to="/contato">Contato</b-nav-item>
-        <b-nav-item href="#" to="/about">Sobre Nós</b-nav-item>
+    <b-collapse id="nav-collapse" is-nav right>
+      <b-navbar-nav right>
+        <b-nav-item href="#" to="/contato" right>Contato</b-nav-item>
+        <b-nav-item href="#" to="/about" right>Sobre Nós</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -30,7 +30,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background: rgb(176, 193, 209);
+  background: lightgray;
   height: 100vh;
   text-align: center;
   color: #2c3e50;
--- src/components/Contato.vue
+++ src/components/Contato.vue
@@ -1,85 +1,77 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>Redes Sociais</h2>
     <ul>
       <li>
         <a
-          href="https://vuejs.org"
+          href="https://www.linkedin.com/in/paula-cristiane-belmiro-26386859/"
           target="_blank"
         >
-          Core Docs
+          Linkedin
         </a>
       </li>
       <li>
         <a
-          href="https://forum.vuejs.org"
+          href="https://github.com/paulabelmiro"
           target="_blank"
         >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
+          Github
         </a>
       </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
+
+    <div class="form">
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show" inline>
+        <label class="mr-sm-2" for="input-group-2">Nome</label>
+        <b-form-group id="input-group-2">
+          <b-form-input
+            id="input-group-2"
+            v-model="form.name"
+            class="mb-2 mr-sm-2 mb-sm-0"
+            placeholder="Digite seu nome"
+            required
+          ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.food"
+          :options="foods"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
+        <b-form-checkbox-group
+          v-model="form.checked"
+          id="checkboxes-4"
+          :aria-describedby="ariaDescribedby"
         >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+          <b-form-checkbox value="me">Check me out</b-form-checkbox>
+          <b-form-checkbox value="that">Check that out</b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    </div>
   </div>
 </template>
 
@@ -88,7 +80,34 @@ export default {
   name: 'Contato',
   data () {
     return {
-      msg: 'Nossos Contatos'
+      msg: 'Nossos Contatos',
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        checked: []
+      },
+      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset (event) {
+      event.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.food = null
+      this.form.checked = []
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
     }
   }
 }
@@ -96,8 +115,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+div {
+  padding: 20px;
+  margin-top: 20px;
+  background-color: lightgray;
+}
 h1, h2 {
-  margin-top: 10px;
   font-weight: normal;
 }
 ul {
